f56d9e6a23c8b271e0ac43a02508c022af9f3dea
Takumi-Hanawa <takumi.hanawa@kinto-technologies.com>
2024-03-19 17:23:26 +0900

    比較する処理を追加

--- pages/date-fns/index.tsx
+++ pages/date-fns/index.tsx
@@ -11,26 +11,59 @@ const DateFns: NextPage = () => {
       "yyyy-MM-dd HH:mm:ss"
     );
   } catch (error) {
-    console.log("formattedDate : ", error);
+    console.error("formattedDateFromISOStr : ", error);
   }
 
   let formattedDateFromParsedISOStr;
   try {
     formattedDateFromParsedISOStr = format(
-      parseISO("2024-01-01T00:00:00"),
+      utcToZonedTime(parseISO("2024-01-01T00:00:00"), "Asia/Tokyo"),
       "yyyy-MM-dd HH:mm:ss"
     );
 
-    console.log(parseISO("2024-01-01T00:00:00"));
+    console.log(
+      "dateFromParsedISOStr : ",
+      utcToZonedTime(parseISO("2024-01-01T00:00:00"), "Asia/Tokyo")
+    );
   } catch (error) {
-    console.log("formattedDate : ", error);
+    console.error("formattedDateFromParsedISOStr : ", error);
+  }
+
+  let formattedDateFromYYYYMMDDStr;
+  try {
+    formattedDateFromYYYYMMDDStr = format(
+      utcToZonedTime("2024-01-01", "Asia/Tokyo"),
+      "yyyy-MM-dd HH:mm:ss"
+    );
+
+    console.log(
+      "dateFromYYYYMMDDStr : ",
+      utcToZonedTime("2024-01-01", "Asia/Tokyo")
+    );
+  } catch (error) {
+    console.error("formattedDateFromYYYYMMDDStr : ", error);
+  }
+
+  let formattedDateFromParsedYYYYMMDDStr;
+  try {
+    formattedDateFromParsedYYYYMMDDStr = format(
+      parseISO("2024-01-01"),
+      "yyyy-MM-dd HH:mm:ss"
+    );
+
+    console.log("dateFromParsedYYYYMMDDStr : ", parseISO("2024-01-01"));
+  } catch (error) {
+    console.error("formattedDateFromParsedYYYYMMDDStr : ", error);
   }
 
   return (
     <div>
       <h1>date-fns Demo</h1>
-      <p>{formattedDateFromISOStr}</p>
-      <p>{formattedDateFromParsedISOStr}</p>
+      <p>From ISO String : {formattedDateFromISOStr}</p>
+      <p>From Parsed ISO String : {formattedDateFromParsedISOStr}</p>
+      <p>
+        From Parsed YYYY-MM-DD String : {formattedDateFromParsedYYYYMMDDStr}
+      </p>
     </div>
   );
 };
