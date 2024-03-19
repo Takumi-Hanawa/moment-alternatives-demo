8e56c678fc5dd9b728a058793248678d999e3fd5
Takumi-Hanawa <takumi.hanawa@kinto-technologies.com>
2024-03-19 17:02:09 +0900

    エラーハンドリング追加

--- pages/date-fns/index.tsx
+++ pages/date-fns/index.tsx
@@ -3,12 +3,24 @@ import { NextPage } from "next";
 import { format, utcToZonedTime } from "date-fns-tz";
 
 const DateFns: NextPage = () => {
-  const jstDateTimeFromStr = utcToZonedTime("2024-01-01", "Asia/Tokyo");
+  let dateObjFromStr;
+  try {
+    dateObjFromStr = utcToZonedTime("2024-01-01", "Asia/Tokyo");
+  } catch (error) {
+    console.log("dateObjFromStr : ", error);
+  }
+
+  let formattedDate;
+  try {
+    formattedDate = format(dateObjFromStr || new Date(), "yyyy-MM-dd HH:mm:ss");
+  } catch (error) {
+    console.log("formattedDate : ", error);
+  }
 
   return (
     <div>
       <h1>date-fns Demo</h1>
-      <p>{format(jstDateTimeFromStr, "yyyy-MM-dd HH:mm:ss")}</p>
+      <p>{formattedDate}</p>
     </div>
   );
 };
