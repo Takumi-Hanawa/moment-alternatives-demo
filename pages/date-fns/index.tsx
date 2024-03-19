7ae94a843ce3cd9bdc00acca35f384124824cfe2
Takumi-Hanawa <takumi.hanawa@kinto-technologies.com>
2024-03-19 18:46:39 +0900

    polyfill追加

--- pages/date-fns/index.tsx
+++ pages/date-fns/index.tsx
@@ -2,6 +2,8 @@ import { NextPage } from "next";
 import { utcToZonedTime } from "date-fns-tz";
 import { format } from "date-fns";
 import "@formatjs/intl-datetimeformat/polyfill";
+import "@formatjs/intl-getcanonicallocales/polyfill";
+import "@formatjs/intl-locale/polyfill";
 
 const DateFns: NextPage = () => {
   const jstDateTime = utcToZonedTime(new Date(), "Asia/Tokyo");
