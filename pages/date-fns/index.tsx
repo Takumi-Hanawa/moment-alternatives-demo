19307aad38a3b7bd5047557e86182d81e378aca4
Takumi-Hanawa <takumi.hanawa@kinto-technologies.com>
2024-03-19 17:51:21 +0900

    検証用ログ追加

--- pages/date-fns/index.tsx
+++ pages/date-fns/index.tsx
@@ -4,6 +4,19 @@ import { format, utcToZonedTime } from "date-fns-tz";
 import { parseISO } from "date-fns";
 
 const DateFns: NextPage = () => {
+  console.log(new Date());
+  console.log(new Date("2024-01-01T00:00:00Z"));
+
+  let formattedDateFromCurrentDate;
+  try {
+    formattedDateFromCurrentDate = format(
+      utcToZonedTime(new Date(), "Asia/Tokyo"),
+      "yyyy-MM-dd HH:mm:ss"
+    );
+  } catch (error) {
+    console.error("formattedDateFromCurrentDate : ", error);
+  }
+
   let formattedDateFromISOStr;
   try {
     formattedDateFromISOStr = format(
@@ -47,6 +60,7 @@ const DateFns: NextPage = () => {
   return (
     <div>
       <h1>date-fns Demo</h1>
+      <p>From Current Date : {formattedDateFromCurrentDate}</p>
       <p>From ISO String : {formattedDateFromISOStr}</p>
       <p>From Parsed ISO String : {formattedDateFromParsedISOStr}</p>
       <p>From YYYY-MM-DD String : {formattedDateFromYYYYMMDDStr}</p>
